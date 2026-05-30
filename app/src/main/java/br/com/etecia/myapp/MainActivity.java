fdc69cb6cc4bab83ffb1cbe849b27fb045141bef
lndsilva <lndsilva@hotmail.com>
2026-05-29 21:54:22 -0300

    criando carregamento dos livros via api externa

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -69,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<FilmeResposta> call, Throwable t) {
+                t.printStackTrace();
                 Toast.makeText(MainActivity.this, "Falha na rede "+t.getMessage(), Toast.LENGTH_SHORT).show();
 
             }
